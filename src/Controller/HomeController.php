fa91b2acfb910a4f2c842157ea5e96e6510aea33
souhail cherif <cherifsouhail474@gmail.com>
2025-01-14 09:57:01 +0100

    Start of To Do List

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController{
+
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function index(TaskRepository $taskRepository): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'tasks' => $taskRepository->findAll(),
+        ]);
+    }
+
+}
+
+
